1c067aeb93be8f4801a273a0da64161f7fad3b8a
giobar22 <baramidze72@gmail.com>
2021-10-26 21:23:51 +0400

    Initial Commit

--- movie/settings.py
+++ movie/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '+6j*v*w-(lwvb$&z-yx2=^)%6zo9dx&@g2!*#^zumtw^3ri41t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'filmebiqartulad.herokuapp.com']
 
 
 # Application definition

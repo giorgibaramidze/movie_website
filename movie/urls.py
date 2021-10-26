a0ff31bcedb064b0b105ec1d940f9d95136df36d
giobar22 <baramidze72@gmail.com>
2021-10-26 22:04:05 +0400

    Initial Commit

--- movie/urls.py
+++ movie/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
